b290272ee62dad196ea3d8f24c48ec02168545cf
Toxic254 <cyrus_g@outlook.com>
2022-10-19 08:56:49 +0300

    Refactored our post list to display time, a tooltip and reactions.

--- src/features/posts/PostList.tsx
+++ src/features/posts/PostList.tsx
@@ -1,40 +1,53 @@
 import React from "react";
 import { useAppSelector } from "../../app/hooks";
 import { Tooltip } from "../../components/ToolTip";
+import TimeAgo from "../utils/TimeAgo";
 import PostAuthor from "./PostAuthor";
 import { selectAllPosts } from "./postSlice";
+import { ReactionButtons } from "./ReactionButtons";
+
+interface Post {
+  id: string;
+  title: string;
+  body: string;
+  userId: string;
+  date: string;
+}
 
 function PostList() {
   const posts = useAppSelector(selectAllPosts);
 
-  interface Post {
-    id: string;
-    title: string;
-    body: string;
-    userId: string;
-  }
+  const orderedPosts = posts
+    .slice()
+    .sort((a, b) => b.date.localeCompare(a.date));
 
-  const renderedPosts = posts
-    .map((post: Post) => (
-      <article
-        key={post.id}
-        className="card mb-3 shadow p-3 mb-5 bg-body border-0 rounded"
-      >
-        <ul>
-          <li>
-            <h4 className="mt-3 mb-2 p-2 display-4">{post.title}</h4>
-            <Tooltip text={post.body}>
-              <p className="mb-3 p-2 display-6 text-muted">
-                <em>{post.body.substring(0, 10)}&hellip;</em>
-              </p>
-            </Tooltip>
+  const renderedPosts = orderedPosts.map((post: Post) => (
+    <article
+      key={post.id}
+      className="card mb-3 shadow p-3 mb-5 bg-body border-0 rounded"
+    >
+      <ul>
+        <li>
+          <h4 className="mt-3 mb-2 p-2 display-4">{post.title}</h4>
+          <Tooltip text={post.body}>
+            <p className="mb-3 p-2 display-6 text-muted">
+              <em>{post.body.substring(0, 10)}&hellip;</em>
+            </p>
+          </Tooltip>
 
+          <hr />
+          <div className="d-flex justify-content-around">
             <PostAuthor userId={post.userId} />
-          </li>
-        </ul>
-      </article>
-    ))
-    .reverse();
+            <TimeAgo timestamp={post.date} />
+          </div>
+
+          <div>
+            <ReactionButtons post={post} />
+          </div>
+        </li>
+      </ul>
+    </article>
+  ));
 
   return (
     <div className="m-5">
