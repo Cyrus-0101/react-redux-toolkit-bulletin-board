e3504c33eb163464c88071e14bd6c8f789e80b68
Toxic254 <cyrus_g@outlook.com>
2022-10-19 08:57:53 +0300

    Added an interface for our reactions, post, added default reactions to the state, added ractionsAdded reducer and exported it.

--- src/features/posts/postSlice.tsx
+++ src/features/posts/postSlice.tsx
@@ -1,17 +1,53 @@
 import { createSlice, nanoid } from "@reduxjs/toolkit";
-
-interface Post {
+import { sub } from "date-fns";
+interface IPost {
   posts: {
     id: string;
     title: string;
     body: string;
     userId: string;
+    date: string;
+    reactions: IReactions;
   }[];
 }
 
+interface IReactions {
+  thumbsUp: number;
+  wow: number;
+  heart: number;
+  rocket: number;
+  coffee: number;
+}
+
 const initialState = [
-  { id: "1", title: "Hello World", body: "This is a post", userId: "1" },
-  { id: "2", title: "Hello World 2", body: "This is a post", userId: "2" },
+  {
+    id: "1",
+    title: "Hello World",
+    body: "This is a post",
+    userId: "1",
+    date: sub(new Date(), { minutes: 30 }).toISOString(),
+    reactions: {
+      thumbsUp: 0,
+      wow: 0,
+      heart: 0,
+      rocket: 0,
+      coffee: 0,
+    },
+  },
+  {
+    id: "2",
+    title: "Hello World 2",
+    body: "This is a post",
+    userId: "2",
+    date: sub(new Date(), { hours: 3 }).toISOString(),
+    reactions: {
+      thumbsUp: 0,
+      wow: 0,
+      heart: 0,
+      rocket: 0,
+      coffee: 0,
+    },
+  },
 ];
 
 const postSlice = createSlice({
@@ -29,15 +65,33 @@ const postSlice = createSlice({
             title,
             body,
             userId,
+            date: new Date().toISOString(),
+            reactions: {
+              thumbsUp: 0,
+              wow: 0,
+              heart: 0,
+              rocket: 0,
+              coffee: 0,
+            },
           },
         };
       },
     },
+    reactionAdded(state, action) {
+      const { postId, reaction } = action.payload;
+
+      // TO-DO: Add generic type to existingPost.
+      const existingPost: any = state.find((post) => post.id === postId);
+
+      if (existingPost) {
+        existingPost.reactions[reaction]++;
+      }
+    },
   },
 });
 
-export const selectAllPosts = (state: Post) => state.posts;
+export const selectAllPosts = (state: IPost) => state.posts;
 
-export const { postAdded } = postSlice.actions;
+export const { postAdded, reactionAdded } = postSlice.actions;
 
 export default postSlice.reducer;
